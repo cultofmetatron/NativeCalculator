fe6c3153414083bec0c61b4e41b39c89d7dea273
Peter de Croos <cultofmetatron@aumlogic.com>
2017-12-22 17:37:30 -0500

    added layout constraints

---
+++ app/src/main/java/com/aumlogic/cultofmetatron/nativecalculator/CalculatorState.kt
@@ -0,0 +1,11 @@
+package com.aumlogic.cultofmetatron.nativecalculator
+
+import java.util.Stack
+
+/**
+ * Created by cultofmetatron on 12/22/17.
+ */
+class CalculatorState {
+    var stack = Stack<Number>()
+
+}
