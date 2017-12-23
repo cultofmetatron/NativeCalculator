68d722495c2c0f58982cf7b9cfee036137ec622b
Peter de Croos <cultofmetatron@aumlogic.com>
2017-12-22 19:20:49 -0500

    text displaying

--- app/src/main/java/com/aumlogic/cultofmetatron/nativecalculator/CalculatorState.kt
+++ app/src/main/java/com/aumlogic/cultofmetatron/nativecalculator/CalculatorState.kt
@@ -6,6 +6,59 @@ import java.util.Stack
  * Created by cultofmetatron on 12/22/17.
  */
 class CalculatorState {
-    var stack = Stack<Number>()
+    var stack = Stack<Float>()
+    var operator: String? = null
+
+    fun getLatestCalculatedValue() : Float {
+        calculate()
+        return stack.peek()
+    }
+
+
+    fun calculate() : CalculatorState {
+        when (stack.count()) {
+            2 -> {
+                // pull both out and operate according to the operand
+                val op2: Float = if (stack.peek() !== null) stack.pop() else 0.toFloat()
+                val op1: Float = if (stack.peek() !== null) stack.pop() else 0.toFloat()
+
+                when (true) {
+                    operator.equals("+") -> {
+                        val value: Float
+                        value = op1 + op2
+                        stack.push(value)
+                        operator = null
+                    }
+                    operator.equals("-") -> {
+                        val value: Float
+                        value = op1 - op2
+                        stack.push(value)
+                        operator = null
+                    }
+                    operator.equals("/") -> {
+                        //handle 0 case later
+                        val value: Float = op1 / op2
+                        stack.push(value)
+                        operator = null
+                    }
+                }
+
+
+                return this
+            }
+            else ->
+                return this
+
+        }
+    }
+
+
+
+
+    //push a number onto the stack
+    fun pushNumber(number: Float): CalculatorState {
+        stack.push(number)
+        return this
+    }
 
 }
--- app/src/main/java/com/aumlogic/cultofmetatron/nativecalculator/MainActivity.kt
+++ app/src/main/java/com/aumlogic/cultofmetatron/nativecalculator/MainActivity.kt
@@ -2,11 +2,75 @@ package com.aumlogic.cultofmetatron.nativecalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    var entry: String = ""
+    var calculatorState = CalculatorState()
+    var lock : Boolean = false
+    var pristine : Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun getDisplayText(): String {
+        return displayText.text.toString()
+    }
+
+    private fun pushDisplayText(num: String) {
+        if (pristine) {
+            pristine = false
+            displayText.text = num
+        } else {
+            displayText.text = displayText.text.toString() + num
+        }
+    }
+
+    fun onZero(view: View) {
+        pushDisplayText("0")
+    }
+
+
+    fun onOne(view: View) {
+        pushDisplayText("1")
+    }
+
+    fun onTwo(view: View) {
+        pushDisplayText("2")
+    }
+
+    fun onThree(view: View) {
+        pushDisplayText("3")
+    }
+
+    fun onFour(view: View) {
+        pushDisplayText("4")
+    }
+
+    fun onFive(view: View) {
+        pushDisplayText("5")
+    }
+
+    fun onSix(view: View) {
+        pushDisplayText("6")
+    }
+
+    fun onSeven(view: View) {
+        pushDisplayText("7")
+    }
+
+    fun onEight(view: View) {
+        pushDisplayText("8")
+    }
+
+    fun onNine(view: View) {
+        pushDisplayText("9")
+    }
+
+
+
 }
